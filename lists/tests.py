53a79630a2a5a9bcd7716412bab3b2e87fa9ca58
drtran <ktran@bemach.com>
2014-07-31 18:29:49 -0400

    Front page HTML now generated a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -14,3 +15,9 @@ class HomePageTest(TestCase):
 		self.assertTrue(response.content.strip().startswith(b'<html>'))
 		self.assertIn(b'<title>To-Do lists</title>', response.content.strip())
 		self.assertTrue(response.content.strip().endswith(b'</html>'))
+		
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		expected_html = render_to_string('home.html')
+		self.assertEqual(response.content.decode(), expected_html)
